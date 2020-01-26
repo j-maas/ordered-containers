62bd11774df477c72393fbb490cd3952772d5d96
Y0hy0h <Y0hy0h@users.noreply.github.com>
2020-01-26 17:46:24 +0100

    Rework tests, implementation, docs

--- src/OrderedDict.elm
+++ src/OrderedDict.elm
@@ -4,15 +4,19 @@ module OrderedDict exposing
     , isEmpty, member, get, size
     , keys, values, toList, fromList
     , map, foldl, foldr, filter, partition
-    , union, intersect, diff
     )
 
-{-| A dictionary type that when iterated or converted to a list, the order of
-entries will be the same as the order they were inserted.
+{-| A dictionary mapping unique keys to values while remembering their
+insertion order. The keys can be any comparable type. This includes
+`Int`, `Float`, `Time`, `Char`, `String`, and tuples or lists
+of comparable types.
 
-As it uses `Dict` under the hood, the keys can only be comparable types, which
-include `Int`, `Float`, `Time`, `Char`, `String`, and tuples or lists of
-comparable types.
+The insertion order is reflected in the functions under "List" and "Transform".
+The list order and the iteration order will be the order of insertion.
+
+The API mirrors the core `Dict`'s API, with exception for the functions
+listed there under "Combine", because these functions do not have
+an obvious way to handle the order between the combined dictionaries.
 
 
 # Dictionaries
@@ -39,85 +43,86 @@ comparable types.
 
 @docs map, foldl, foldr, filter, partition
 
-
-# Combine
-
-@docs union, intersect, diff
-
 -}
 
 import Dict exposing (Dict)
 import List
 
 
-{-| A type of `Dict` that is iterated by the order of insertion.
+{-| A dictionary of keys and values that remembers the order of insertion.
 -}
 type OrderedDict comparable v
-    = OrderedDict (List comparable) (Dict comparable v)
+    = OrderedDict (OrdDict comparable v)
+
+
+type alias OrdDict comparable v =
+    { order : List comparable
+    , dict : Dict comparable v
+    }
 
 
 {-| Create an empty dictionary.
 -}
 empty : OrderedDict comparable v
 empty =
-    OrderedDict [] Dict.empty
-
-
-{-| Check if a dictionary is empty.
--}
-isEmpty : OrderedDict comparable v -> Bool
-isEmpty odict =
-    odict == empty
-
-
-{-| Get the value associated with a key. Return `Nothing` if the key is not found.
--}
-get : comparable -> OrderedDict comparable v -> Maybe v
-get key (OrderedDict _ dict) =
-    Dict.get key dict
-
-
-{-| Check if a key is in a dictionary.
--}
-member : comparable -> OrderedDict comparable v -> Bool
-member key (OrderedDict _ dict) =
-    Dict.member key dict
-
-
-{-| Get the number of key-value pairs in the dictionary.
--}
-size : OrderedDict comparable v -> Int
-size (OrderedDict list _) =
-    List.length list
+    OrderedDict { order = [], dict = Dict.empty }
 
 
 {-| Create a dictionary with one key-value pair.
 -}
 singleton : comparable -> v -> OrderedDict comparable v
 singleton key value =
-    insert key value empty
+    OrderedDict
+        { order = [ key ]
+        , dict = Dict.singleton key value
+        }
 
 
-{-| Insert a key-value pair into a dictionary. Replace the value if the key
-already exists.
+{-| Insert a key-value pair into a dictionary. If the key already exists,
+the old value will be forgotten and the new value will be inserted at the end.
 -}
 insert : comparable -> v -> OrderedDict comparable v -> OrderedDict comparable v
-insert key value odict =
-    update key (always (Just value)) odict
+insert key value (OrderedDict orderedDict) =
+    let
+        filteredOrder =
+            if Dict.member key orderedDict.dict then
+                List.filter (\k -> k /= key) orderedDict.order
+
+            else
+                orderedDict.order
+
+        newOrder =
+            filteredOrder ++ [ key ]
+    in
+    OrderedDict
+        { order = newOrder
+        , dict = Dict.insert key value orderedDict.dict
+        }
 
 
-{-| Remove a key-value pair from a dictionary. If the key doesn't exist,
+{-| Remove a key-value pair from a dictionary. If the key is not found,
 no changes are made.
 -}
 remove : comparable -> OrderedDict comparable v -> OrderedDict comparable v
-remove key odict =
-    update key (always Nothing) odict
+remove key (OrderedDict orderedDict) =
+    OrderedDict
+        { order =
+            if Dict.member key orderedDict.dict then
+                List.filter (\k -> k /= key) orderedDict.order
+
+            else
+                orderedDict.order
+        , dict = Dict.remove key orderedDict.dict
+        }
 
 
-{-| Update the value of a specific key with an updater function.
+{-| Update the value of a dictionary for a specific key with a given function.
 
-Since the key may not exist before or after the update, the updater function
-takes a `Maybe v` and returns a `Maybe v`.
+If a value exists for the key and the passed function returns a `Just v`,
+the value will be replaced, keeping its order. If it did not exist, the
+new value will be added to the end.
+
+When the function returns a `Nothing`, the key is removed.
 
 -}
 update :
@@ -125,104 +130,58 @@ update :
     -> (Maybe v -> Maybe v)
     -> OrderedDict comparable v
     -> OrderedDict comparable v
-update key updater (OrderedDict list dict) =
-    let
-        newDict =
-            Dict.update key updater dict
-
-        newList =
-            case updater (Dict.get key dict) of
-                Just _ ->
-                    if List.member key list then
-                        list
-
-                    else
-                        list ++ [ key ]
+update key alter ((OrderedDict orderedDict) as original) =
+    case Dict.get key orderedDict.dict of
+        Just oldItem ->
+            case alter (Just oldItem) of
+                Just newItem ->
+                    OrderedDict
+                        { order = orderedDict.order
+                        , dict = Dict.update key (always (Just newItem)) orderedDict.dict
+                        }
 
                 Nothing ->
-                    if List.member key list then
-                        List.filter (\k -> k /= key) list
+                    remove key original
 
-                    else
-                        list
-    in
-    OrderedDict newList newDict
+        Nothing ->
+            case alter Nothing of
+                Just newItem ->
+                    insert key newItem original
 
-
-
--- TRANSFORM
+                Nothing ->
+                    original
 
 
-{-| Apply a function to all values in a dictionary.
+
+-- QUERY
+
+
+{-| Determine if a dictionary is empty.
 -}
-map : (comparable -> a -> b) -> OrderedDict comparable a -> OrderedDict comparable b
-map f (OrderedDict list dict) =
-    OrderedDict list (Dict.map f dict)
+isEmpty : OrderedDict comparable v -> Bool
+isEmpty (OrderedDict orderedDict) =
+    Dict.isEmpty orderedDict.dict
 
 
-reducer :
-    Dict comparable v
-    -> (comparable -> v -> b -> b)
-    -> (comparable -> b -> b)
-reducer dict f =
-    \key acc ->
-        case Dict.get key dict of
-            Just value ->
-                f key value acc
-
-            Nothing ->
-                acc
-
-
-{-| Fold the key-value pairs in a dictionary, in insertion order.
+{-| Determine if a key is in a dictionary.
 -}
-foldl : (comparable -> v -> b -> b) -> b -> OrderedDict comparable v -> b
-foldl f acc (OrderedDict list dict) =
-    List.foldl (reducer dict f) acc list
+member : comparable -> OrderedDict comparable v -> Bool
+member key (OrderedDict orderedDict) =
+    Dict.member key orderedDict.dict
 
 
-{-| Fold the key-value pairs in a dictionary, in reverse insertion order.
+{-| Get the value associated with a key. If the key is not found, return Nothing.
 -}
-foldr : (comparable -> v -> b -> b) -> b -> OrderedDict comparable v -> b
-foldr f acc (OrderedDict list dict) =
-    List.foldr (reducer dict f) acc list
+get : comparable -> OrderedDict comparable v -> Maybe v
+get key (OrderedDict orderedDict) =
+    Dict.get key orderedDict.dict
 
 
-{-| Return a new `OrderedDict` that only contains entries that satisfy a predicate.
+{-| Determine the number of key-value pairs in the dictionary.
 -}
-filter :
-    (comparable -> v -> Bool)
-    -> OrderedDict comparable v
-    -> OrderedDict comparable v
-filter predicate dictionary =
-    let
-        add key value odict =
-            if predicate key value then
-                insert key value odict
-
-            else
-                odict
-    in
-    foldl add empty dictionary
-
-
-{-| Split a `OrderedDict` to a tuple of `OrderedDict`. The first contains all
-entries that satisfy the predicate, and the second contains the rest.
--}
-partition :
-    (comparable -> v -> Bool)
-    -> OrderedDict comparable v
-    -> ( OrderedDict comparable v, OrderedDict comparable v )
-partition predicate dictionary =
-    let
-        add key value ( odict1, odict2 ) =
-            if predicate key value then
-                ( insert key value odict1, odict2 )
-
-            else
-                ( odict1, insert key value odict2 )
-    in
-    foldl add ( empty, empty ) dictionary
+size : OrderedDict comparable v -> Int
+size (OrderedDict orderedDict) =
+    Dict.size orderedDict.dict
 
 
 
@@ -232,22 +191,27 @@ partition predicate dictionary =
 {-| Get all of the keys in a dictionary, in insertion order.
 -}
 keys : OrderedDict comparable v -> List comparable
-keys (OrderedDict list _) =
-    list
+keys (OrderedDict orderedDict) =
+    orderedDict.order
 
 
 {-| Get all of the values in a dictionary, in insertion order.
 -}
 values : OrderedDict comparable v -> List v
-values (OrderedDict list dict) =
-    List.filterMap (\key -> Dict.get key dict) list
+values (OrderedDict orderedDict) =
+    List.filterMap (\key -> Dict.get key orderedDict.dict) orderedDict.order
 
 
 {-| Convert a dictionary into an association list of key-value pairs, in insertion order.
 -}
 toList : OrderedDict comparable v -> List ( comparable, v )
-toList (OrderedDict list dict) =
-    List.map2 (\a b -> ( a, b )) list (values <| OrderedDict list dict)
+toList (OrderedDict orderedDict) =
+    List.filterMap
+        (\key ->
+            Dict.get key orderedDict.dict
+                |> Maybe.map (\value -> ( key, value ))
+        )
+        orderedDict.order
 
 
 {-| Convert an association list into a dictionary.
@@ -255,52 +219,76 @@ toList (OrderedDict list dict) =
 fromList : List ( comparable, v ) -> OrderedDict comparable v
 fromList assocs =
     let
-        ( list, _ ) =
+        ( order, _ ) =
             List.unzip assocs
 
         dict =
             Dict.fromList assocs
     in
-    OrderedDict list dict
+    OrderedDict { order = order, dict = dict }
 
 
 
--- COMBINE
+-- TRANSFORM
 
 
-{-| Combine two dictionaries. If there is a collision, preference is given
-to the first dictionary.
-
-    union (singleton "x" 1) (singleton "x" 2) == singleton "x" 1
-
+{-| Apply a function to all values in a dictionary.
 -}
-union : OrderedDict comparable v -> OrderedDict comparable v -> OrderedDict comparable v
-union odict1 odict2 =
-    let
-        unionReducer =
-            \k v acc ->
-                if member k odict1 then
-                    acc
-
-                else
-                    insert k v acc
-    in
-    foldl unionReducer odict1 odict2
+map : (comparable -> a -> b) -> OrderedDict comparable a -> OrderedDict comparable b
+map func (OrderedDict orderedDict) =
+    OrderedDict { order = orderedDict.order, dict = Dict.map func orderedDict.dict }
 
 
-{-| Keep a key-value pair when its key appears in the second dictionary.
-Preference is given to values in the first dictionary.
+{-| Fold over the key-value pairs in a dictionary in insertion order.
 -}
-intersect :
-    OrderedDict comparable v
+foldl : (comparable -> v -> b -> b) -> b -> OrderedDict comparable v -> b
+foldl func acc (OrderedDict orderedDict) =
+    List.foldl (reducer orderedDict.dict func) acc orderedDict.order
+
+
+{-| Fold over the key-value pairs in a dictionary in reverse insertion order.
+-}
+foldr : (comparable -> v -> b -> b) -> b -> OrderedDict comparable v -> b
+foldr func acc (OrderedDict orderedDict) =
+    List.foldr (reducer orderedDict.dict func) acc orderedDict.order
+
+
+reducer :
+    Dict comparable v
+    -> (comparable -> v -> b -> b)
+    -> (comparable -> b -> b)
+reducer dict func =
+    \key acc ->
+        case Dict.get key dict of
+            Just value ->
+                func key value acc
+
+            Nothing ->
+                acc
+
+
+{-| Keep only the key-value pairs that pass the given test.
+-}
+filter :
+    (comparable -> v -> Bool)
     -> OrderedDict comparable v
     -> OrderedDict comparable v
-intersect odict1 odict2 =
-    filter (\k _ -> member k odict2) odict1
+filter predicate original =
+    toList original
+        |> List.filter (\( k, v ) -> predicate k v)
+        |> fromList
 
 
-{-| Keep a key-value pair when its key does not appear in the second dictionary.
+{-| Partition a dictionary according to some test. The first dictionary
+contains all key-value pairs which passed the test, and the second
+contains the pairs that did not. The order is preserved in these new
+dictionaries.
 -}
-diff : OrderedDict comparable v -> OrderedDict comparable v -> OrderedDict comparable v
-diff odict1 odict2 =
-    foldl (\k v t -> remove k t) odict1 odict2
+partition :
+    (comparable -> v -> Bool)
+    -> OrderedDict comparable v
+    -> ( OrderedDict comparable v, OrderedDict comparable v )
+partition predicate original =
+    toList original
+        |> List.partition (\( k, v ) -> predicate k v)
+        |> Tuple.mapBoth fromList fromList
